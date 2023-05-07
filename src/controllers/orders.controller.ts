9afb891442589fc68a44a883883eeb4352cef560
David Forero <54685136+David-forero@users.noreply.github.com>
2023-05-07 16:20:52 -0400

    creacion de la orden

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { OrdersService } from 'src/services';
 
-@Controller('orders')
-export class OrdersController {}
+@Controller('')
+export class OrdersController {
+  constructor(private readonly orderService: OrdersService) {}
+
+  @Post('/create-checkout-session')
+  async createOrder(@Res() res, @Body() body) {
+    const order = await this.orderService.checkOutWithStripe(body);
+
+    return res.status(HttpStatus.OK).json({
+        data: order,
+        status: 200,
+        message: 'Success'
+    })
+  }
+}
--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,62 +1,69 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import stripe from 'stripe'
+import Stripe from 'stripe';
+import { runInThisContext } from 'vm';
 
 @Injectable()
 export class OrdersService {
-    constructor(private prisma: PrismaService){}
+  private stripe;
 
-   checkOutWithStripe({items, email}){
-    const transformedItems = items.map(item => ({
-        quantity: 1,
-        price_data: {
-            currency: 'usd',
-            unit_amount: item.price * 100,
-            product_data: {
-                name: item.title,
-                description: item.description,
-                images: [item.image]
-            },
+  constructor(private prisma: PrismaService) {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2022-11-15',
+    });
+  }
 
-        }
+  async checkOutWithStripe(body: any): Promise<any> {
+    
+    const transformedItems = body.items.map((item) => ({
+      quantity: 1,
+      price_data: {
+        currency: 'usd',
+        unit_amount: item.price * 100,
+        product_data: {
+          name: item.title,
+          description: item.description,
+          images: [item.imgUrl],
+        },
+      },
     }));
 
-    // const session = await stripe.checkout.sessions.create({
-    //     payment_method_types: ['card'],
-    //     shipping_options: [
-    //         {
-    //             shipping_rate_data: {
-    //               type: 'fixed_amount',
-    //               fixed_amount: {amount: 0, currency: 'usd'},
-    //               display_name: 'Delivery Gratis',
-    //               delivery_estimate: {
-    //                 minimum: {unit: 'business_day', value: 5},
-    //                 maximum: {unit: 'business_day', value: 7},
-    //               },
-    //             },
-    //           },
-    //           {
-    //             shipping_rate_data: {
-    //               type: 'fixed_amount',
-    //               fixed_amount: {amount: 1500, currency: 'usd'},
-    //               display_name: 'Next day air',
-    //               delivery_estimate: {
-    //                 minimum: {unit: 'business_day', value: 1},
-    //                 maximum: {unit: 'business_day', value: 1},
-    //               },
-    //             },
-    //           },
-    //       ],
-    //     line_items: transformedItems,
-    //     mode: 'payment',
-    //     success_url: `${process.env.HOST}/success`,
-    //     cancel_url: `${process.env.HOST}/checkout`,
-    //     metadata: {
-    //         email,
-    //         images: JSON.stringify(items.map(item => item.image))
-    //     }
-    // })
-   }
+    const session = await this.stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                  type: 'fixed_amount',
+                  fixed_amount: {amount: 0, currency: 'usd'},
+                  display_name: 'Delivery Gratis',
+                  delivery_estimate: {
+                    minimum: {unit: 'business_day', value: 5},
+                    maximum: {unit: 'business_day', value: 7},
+                  },
+                },
+              },
+              {
+                shipping_rate_data: {
+                  type: 'fixed_amount',
+                  fixed_amount: {amount: 1500, currency: 'usd'},
+                  display_name: 'Delivery Normal',
+                  delivery_estimate: {
+                    minimum: {unit: 'business_day', value: 1},
+                    maximum: {unit: 'business_day', value: 2},
+                  },
+                },
+              },
+          ],
+        line_items: transformedItems,
+        mode: 'payment',
+        success_url: `${process.env.HOST_WEB}/orders`,
+        cancel_url: `${process.env.HOST_WEB}`,
+        metadata: {
+            email: body.email,
+            images: JSON.stringify(body.items.map(item => item.image))
+        }
+    })
 
-
+    return session
+  }
 }
